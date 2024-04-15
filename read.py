9ff7f90be101df28775ac0ca0f574cae370cd7d8
sorteggideifiori <101125532+sorteggi@users.noreply.github.com>
2024-04-15 21:22:32 +0200

    Update read.py

--- read.py
+++ read.py
@@ -5,15 +5,23 @@ import serial
 
 FBConn = firebase.FirebaseApplication('https://came-54f87-default-rtdb.europe-west1.firebasedatabase.app/', None)
 
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = "COM3"
+ser.open()
+
 while True:
 
     myGetResults = FBConn.get('/position/', None)
     
     microbitdata = str(myGetResults['xpos'])
     print (microbitdata)
-    microbitdata = str(myGetResults['ypos'])
-    print (microbitdata)
+    ser.write(microbitdata.encode('UTF-8') + b"\n")
+    #microbitdata = str(myGetResults['ypos'])
+    #print (microbitdata)
     
-    time.sleep(1)
+    time.sleep(2)
 
-    
+
+ser.close()
+
